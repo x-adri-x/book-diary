4f2796dc5a8b45f3b41be1eead0253f07db0a482
Adrienn Lenart <adriennlenart@yahoo.com>
2024-10-26 03:07:28 +0200

    feat: signup page

---
+++ app/signup/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import React from 'react'
+import SignUpForm from '@/app/ui/signup-form'
+
+export default function Page() {
+  return (
+    <div className='h-full'>
+      <SignUpForm />
+    </div>
+  )
+}
